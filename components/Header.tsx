da5a15bad9bf30c770e076b9451b2ce159a583dd
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-01-01 20:29:45 +0700

    added redux: basketSlice

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,12 @@ import {
 	ShoppingBagIcon,
 	UserIcon,
 } from "@heroicons/react/outline";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Header = () => {
 	const session = false;
+	const items = useSelector((state: RootState) => state.basket.items);
 
 	return (
 		<header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
@@ -16,7 +19,6 @@ const Header = () => {
 				<Link href="/">
 					<div className="realtive  h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
 						<Image
-							// src="https://instagram.fbdo1-1.fna.fbcdn.net/v/t51.2885-15/321872267_1029307441794025_477004283151965997_n.jpg?stp=dst-jpg_e35&amp;_nc_ht=instagram.fbdo1-1.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_ohc=9bhzELkMysAAX_nTkCP&amp;edm=ALQROFkBAAAA&amp;ccb=7-5&amp;ig_cache_key=MzAwMDY5NDg1MTI0NDA3MjU0MA%3D%3D.2-ccb7-5&amp;oh=00_AfBQjwMTwhWZEvkfw4NDueFKoq25t4YHy-NlzE1hSNDf_A&amp;oe=63ACEC06&amp;_nc_sid=30a2ef"
 							src="https://rb.gy/vsvv2o"
 							alt=""
 							width="60"
@@ -38,7 +40,7 @@ const Header = () => {
 				<Link href="/checkout">
 					<div className="relative cursor-pointer">
 						<span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
-							5
+							{items.length}
 						</span>
 						<ShoppingBagIcon className="headerIcon" />
 					</div>
